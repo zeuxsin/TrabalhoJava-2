08e4c0492c8cd5ff1fb60e6cdf77669bc281384c
zeuxsin <144070851+zeuxsin@users.noreply.github.com>
2024-11-26 19:42:58 -0300

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -2,9 +2,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/SistemaPassagens";
-    private static final String USER = "root";
-    private static final String PASSWORD = "password";
+    private static final String URL = "jdbc:sqlite:database.db";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() {
         try {
